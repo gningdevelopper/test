07de26b8150353315d1dead3d5e375a919c19fdf
unknown <ousseynougning12@gmail.com>
2018-11-03 11:04:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# edacy-javaee-courses
+This repo contains sources for JEE training given at Edacy Makersday
+
+
+# module.xml Mysql driver
+
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<module xmlns="urn:jboss:module:1.1" name="com.mysql">
+    <resources>
+        <resource-root path="mysql-connector-java-5.1.45-bin.jar"/>              
+    </resources>
+    <dependencies>
+        <module name="java"/>
+        <module name="javax.transaction.api"/>
+    </dependencies>
+</module>
+```
+
+```
+<driver name="mysql" module="com.mysql">
+                        <driver-class>com.mysql.jdbc.Driver</driver-class>
+                    </driver>
+```
---
+++ cohort-managment-jee7/cohort-business/src/main/java/sn/edacy/business/CohortService.java
@@ -0,0 +1,31 @@
+package sn.edacy.business;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import sn.edacy.model.Cohort;
+import sn.edacy.model.Talent;
+
+@Stateless
+public class CohortService {
+          // modification sur cette classe avec l'editeur vim
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void saveCohort(Cohort cohort) {
+		
+	}
+	
+	/**
+	 * @return list of cohorts
+	 */
+	public List<Cohort> getAllCohort() {
+		Query query =  entityManager.createQuery("select c from Cohort c");
+		return query.getResultList();
+	}
+// j'ai effectuer une modification 
+}
